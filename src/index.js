a949fc9d863aa122dcbe405f8229550ac1ead4d0
jean pierre <kreyesf@unamad.edu.pe>
2023-07-22 21:50:07 -0500

    add mail confirmation voto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 
 app.use(express.urlencoded({extended: false}));
 
--- src/router/router.js
+++ src/router/router.js
@@ -2,8 +2,33 @@ const { Router } = require('express');
 const router = Router();
 
 const ruta = require('../controller/controller')
+var eventData = {
+}
 
 router.post('/send-email', ruta.sendMail);
-router.post('/send-email2', ruta.sendMail2);
+router.post('/confirmation', ruta.sendMail2);
 router.get('/send-email_get/:email', ruta.sendMailGet)
+router.get('/events', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Envía eventos al cliente cada 2 segundos
+  setInterval(() => {
+
+    res.write(`event: data\n`);
+    res.write(`data: ${JSON.stringify(eventData)}\n\n`);
+  }, 2000);
+});
+
+router.post("/eventsPost", (req, res) => {
+  const { datetime, temperature } = req.body;
+  console.log(datetime + " " + temperature)
+  eventData = {
+    datetime,
+    temperature
+  }
+  res.json("succefull")
+})
 module.exports = router;
